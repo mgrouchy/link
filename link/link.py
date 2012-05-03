e0ee754ef137ee1eecc83c10c56ef61eab60ec76
Mike Grouchy <mgrouchy@gmail.com>
2012-05-02 21:25:00 -0400

    bunch of cleanup, still some bugs in the get access token process

--- link/link.py
+++ link/link.py
@@ -15,14 +15,15 @@ class LinkError(Exception):
 class Link(object):
     """
     Link is the main interface for the linkedin Api
-        params:
-            linkedin_key:
-            linkedin_secret:
-            oauth_token:
-            oauth_token_secret:
     """
     def __init__(self, linkedin_key=None, linkedin_secret=None,
                      oauth_token=None, oauth_token_secret=None):
+        """
+        :param linkedin_key:
+        :param linkedin_secret:
+        :param oauth_token:
+        :param oauth_token_secret:
+        """
 
         # api urls
         self.base_api_url = 'https://api.linkedin.com'
@@ -39,14 +40,11 @@ class Link(object):
         OAuthHook.consumer_key = self.linkedin_key
         OAuthHook.consumer_secret = self.linkedin_secret
 
-        self.oauth_token = oauth_token or None
-        self.oauth_token_secret = oauth_token_secret or None
+        self.oauth_token = oauth_token
+        self.oauth_token_secret = oauth_token_secret
 
-        if self.oauth_token and self.oauth_token_secret:
-            oauth_hook = OAuthHook(oauth_token=self.oauth_token,
-                                   oauth_token_secret=self.oauth_token_secret)
-        else:
-            oauth_hook = OAuthHook()
+        oauth_hook = OAuthHook(access_token=self.oauth_token,
+                               access_token_secret=self.oauth_token_secret)
 
         self.client = requests.session(hooks={'pre_request': oauth_hook})
         self.request_token = None
@@ -64,14 +62,16 @@ class Link(object):
 
     def request(self, endpoint, method='GET', data=None, field_selectors=None):
         """
-       ` Execute a Linkedin API request
-        params:
-            endpoint:
-            method:
-            data:
-            field_selectors:
+        Execute a Linkedin API request
+        :param endpoint:
+        :param method:
+        :param data:
+        :param field_selectors:
         """
         method = method.lower()
+
+        headers = {'x-li-format': 'json'}
+
         if method not in ['get', 'post', 'delete', 'put']:
             raise LinkError
 
@@ -89,31 +89,35 @@ class Link(object):
         if not _client:
             raise LinkError
 
-        response = _client(endpoint, data=data)
+        response = _client(endpoint, data=data, headers=headers)
 
         content_dict = simplejson.loads(response.content)
         return content_dict
 
-    def get_auth_url(self, token, callback=None):
+    def get_authorization_url(self, token, callback=None):
         """
         Get an Authorization url developers can put in their app to get user to
         authorize a linkedin connection
+        :param token
+        :param callback
         """
-        params = {'oauth_token': token}
+        qs = 'oauth_token=%s' % token
         if callback:
-            params['oauth_callback'] = callback
-
-        return '%s?%s' % (self.authorize_url, urllib.urlencode(params))
+            qs = '%s&oauth_callback=%s' % (qs, urllib.urlencode(callback))
+        return '%s?%s' % (self.authorize_url, qs)
 
     def get_request_token(self, callback=None):
         """
         Get a linkedin request token. Return the tokens as a dict, also append
         the authorization url, cause you likely want it.
+        :param callback
         """
         response = self.client.get(self.request_token_url)
 
         self.request_token = parse_qs(response.content)
-        self.request_token['url'] = self.get_auth_url(self.request_token['oauth_token'], callback=callback)
+
+        self.request_token['url'] = self.get_authorization_url(
+                    self.request_token['oauth_token'][0], callback=callback)
         return self.request_token
 
     def get_access_token(self, verifier):
@@ -121,9 +125,11 @@ class Link(object):
         Given a Verifier, get the linkedin access tokens
         """
         response = self.client.post(self.access_token_url,
-                data={'oauth_verifier': verifier, 'oauth_token': self.oauth_token})
+                data={'oauth_verifier': verifier})
 
         access_token = parse_qs(response.content)
+        print access_token
+
         self.oauth_token = access_token['oauth_token']
         self.oauth_token_secret = access_token['oauth_token_secret']
         return access_token
