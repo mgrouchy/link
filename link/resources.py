7204129c35066f9410d635826d24aff300e856d6
Mike Grouchy <mgrouchy@gmail.com>
2012-05-03 11:08:12 -0400

    Added Endpoints for: * Member Profiles * Connections * People Search * Companies * Company Search * Company Follow and Suggestions

--- link/resources.py
+++ link/resources.py
@@ -5,10 +5,43 @@ ENDPOINTS = {
         'profile': {
             'method': 'GET',
             'url': 'https://api.linkedin.com/v1/people/%(named_params)s%(field_selectors)s',
+            '__doc__': 'see https://developer.linkedin.com/documents/profile-api'
         },
-        # 'connections': {
-        #    'method': 'GET',
-        #    'url': '%s/v1/people/%s/connections' % BASE_API,
-        #}
+         'connections': {
+            'method': 'GET',
+            'url': 'https://api.linkedin.com/v1/people/%(named_params)s/connections%(field_selectors)s',
+            '__doc__': 'see https://developer.linkedin.com/documents/connections-api ',
+        },
+        'people_search': {
+            'method': 'GET',
+            'url': 'https://api.linkedin.com/v1/people-search',
+            '__doc__': 'see https://developer.linkedin.com/documents/people-search-api',
+        },
+        'company_search': {
+            'method': 'GET',
+            'url': 'https://api.linkedin.com/v1/company-search%(field_selectors)s',
+            '__doc__': 'see https://developer.linkedin.com/documents/company-search'
+        },
+        'company_follow_list': {
+            'method': 'GET',
+            'url': 'https://api.linkedin.com/v1/people/%(named_params)s/following/companies',
+            '__doc__': 'see https://developer.linkedin.com/documents/company-follow-and-suggestions',
+        },
+        'company_follow': {
+            'method': 'POST',
+            'url': 'https://api.linkedin.com/v1/people/~/following/companies',
+            '__doc__': 'see https://developer.linkedin.com/documents/company-follow-and-suggestions',
+        },
+        'company_unfollow': {
+            'method': 'DELETE',
+            'url': 'http://api.linkedin.com/v1/people/~/following/companies/%(named_params)s',
+            '__doc__': 'see https://developer.linkedin.com/documents/company-follow-and-suggestions',
+        },
+        'company_follow_suggestions': {
+            'method': 'GET',
+            'url': 'http://api.linkedin.com/v1/people/~/suggestions/to-follow/companies',
+            '__doc__': 'see https://developer.linkedin.com/documents/company-follow-and-suggestions',
+        },
+
 
 }
